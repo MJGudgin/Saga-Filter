0b7aa51a88a6d11b1a113ac3c7240479eca03674
Matthew <mjudgin@live.com>
2024-12-11 15:13:47 -0700

    Initial commit of Saga filter

---
+++ script.ps1
@@ -0,0 +1,5 @@
+$file = "Saga.filter"
+
+Copy-Item *.filter 'C:\Users\mjgud\OneDrive\Documents\My Games\Path of Exile 2\'
+Copy-Item *.wav 'C:\Users\mjgud\OneDrive\Documents\My Games\Path of Exile 2\'
+Copy-Item *.wav 'C:\Users\mjgud\OneDrive\Documents\My Games\Path of Exile 2\'
